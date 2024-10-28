e4b31215b8d7451910d099e93ed8f012fbed5e54
shankar-b-s <144048522+shankar-b-s@users.noreply.github.com>
2024-10-28 23:33:37 +0530

    tfv completed

--- src/components/tfvtabcomp/tfvheader/Tfvheader.jsx
+++ src/components/tfvtabcomp/tfvheader/Tfvheader.jsx
@@ -3,11 +3,11 @@ const Tfvheader = () => {
   return ( 
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center text-nowrap my-1">
-        <p className="text-[41px] md:text-6xl lg:text-[196px] py-1 md:py-2">
+        <p className="text-[46px] md:text-6xl lg:text-[196px] md:py-2">
           TEACH FOR VELLORE
         </p>
       </div>
-      <div className="border-t-4 md:border-t-8 border-b-4 md:border-b-8 border-black w-11/12 py-2 flex justify-between items-center text-[22px] md:text-4xl lg:text-[58px] my-2 text-nowrap">
+      <div className="py-[4px] md:py-[6px] border-t-4 md:border-t-8 border-b-4 md:border-b-8 border-black w-11/12 flex justify-between items-center text-[22px] md:text-4xl lg:text-[58px] text-nowrap">
         <div>
           OUR WAY OF {" "} 
           <span className="hover:text-[#27A5EF] transition-all duration-150 ease-in-out">
@@ -20,7 +20,7 @@ const Tfvheader = () => {
             alt="prop"
             width={100}
             height={100}
-            className="w-[38px] h-[38px] md:w-24 md:h-24 lg:w-[72px] lg:h-[72px] lg:min-w-30 lg:min-w-30"
+            className="w-[30px] h-[30px] md:w-24 md:h-24 lg:w-[72px] lg:h-[72px] lg:min-w-30 lg:min-w-30"
           />
         </div>
       </div>
---
+++ src/components/tfvtabcomp/tfvimagepart/Tfvimagepart.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+const Tfvimagepart = () => {
+  return ( 
+    <div className="-my-4 md:-my-10">
+      <div className="flex md:mx-16 md:ml-20">
+        <Image src="/Pictures/tfv/polaroids5.png" width={300} height={300} alt="tfvimg" className="-mx-3 md:-mx-6 grayscale hover:animate-rotate-right hover:duration-400 hover:grayscale-0 transition-all duration-500 h-[90px] w-[90px] md:h-[350px] md:w-[350px]"/>
+        <Image src="/Pictures/tfv/polaroids6.png" width={300} height={300} alt="tfvimg" className="-mx-3 md:-mx-6 grayscale hover:animate-rotate-left hover:duration-400  hover:grayscale-0 transition-all duration-500 h-[90px] w-[90px] md:h-[350px] md:w-[350px]"/>
+        <Image src="/Pictures/tfv/polaroids7.png" width={300} height={300} alt="tfvimg" className="-mx-3 md:-mx-6 grayscale hover:animate-rotate-right hover:duration-400 hover:grayscale-0 transition-all duration-500 h-[90px] w-[90px] md:h-[350px] md:w-[350px]"/>
+        <Image src="/Pictures/tfv/polaroids8.png" width={300} height={300} alt="tfvimg" className="-mx-3 md:-mx-6 grayscale hover:animate-rotate-left hover:duration-400 hover:grayscale-0 transition-all duration-500 h-[90px] w-[90px] md:h-[350px] md:w-[350px]"/>
+        <Image src="/Pictures/tfv/polaroids9.png" width={300} height={300} alt="tfvimg" className="-mx-3 md:-mx-6 grayscale hover:animate-rotate-right hover:duration-400 hover:grayscale-0 transition-all duration-500 h-[90px] w-[90px] md:h-[350px] md:w-[350px]"/>
+      </div>
+      <div className="flex  md:flex md:flex-nowrap mx-4 md:ml-40 md:mx-36 -my-7 md:-my-[120px]">
+        <Image src="/Pictures/tfv/polaroids2.png" width={300} height={300} alt="tfvimg" className="-mx-3 md:-mx-6 grayscale hover:animate-rotate-left hover:duration-400 hover:grayscale-0 transition-all duration-500 h-[90px] w-[90px] md:h-[350px] md:w-[350px]"/>
+        <Image src="/Pictures/tfv/polaroids1.png" width={300} height={300} alt="tfvimg" className="-mx-3 md:-mx-6 grayscale hover:animate-rotate-right hover:duration-400 hover:grayscale-0 transition-all duration-500 h-[90px] w-[90px] md:h-[350px] md:w-[350px]"/>
+        <Image src="/Pictures/tfv/polaroids4.png" width={300} height={300} alt="tfvimg" className="-mx-3 md:-mx-6 grayscale hover:animate-rotate-left hover:duration-400 hover:grayscale-0 transition-all duration-500 h-[90px] w-[90px] md:h-[350px] md:w-[350px]"/>
+        <Image src="/Pictures/tfv/polaroids3.png" width={300} height={300} alt="tfvimg" className="-mx-3 md:-mx-6 grayscale hover:animate-rotate-right hover:duration-400 hover:grayscale-0 transition-all duration-500 h-[90px] w-[90px] md:h-[350px] md:w-[350px]"/>
+      </div>
+    </div>
+   );
+}
+ 
+export default Tfvimagepart;
