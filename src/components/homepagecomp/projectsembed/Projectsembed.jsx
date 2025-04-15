86f8bc4ccd25645d17f33e94fa98ba0407b9518f
Harsh Kumar Sinha <hksinha960@gmail.com>
2025-04-16 02:00:02 +0530

    Project Embed

--- src/components/homepagecomp/projectsembed/Projectsembed.jsx
+++ src/components/homepagecomp/projectsembed/Projectsembed.jsx
@@ -10,12 +10,9 @@ const Projectsembed = () => {
         ))}
       </div>
       <div className="relative z-20 flex justify-center items-center p-4 border-4 border-white shadow-[0_0_15px_rgba(255,255,255,0.6)] transition-all duration-300 hover:border-[#27A5EF] hover:shadow-[0_0_25px_rgba(39,165,239,0.5)]">
-        <Image
-          alt="project poster"
-          src="/Pictures/projectsupcomingposter.png"
-          width={960}
-          height={501}
-          className="object-contain w-[300px] h-auto md:w-[960px]"
+        <iframe
+          src="https://projects.istevit.in"
+          className="object-contain w-[300px] h-[501px] md:w-[960px]"
         />
       </div>
     </div>
